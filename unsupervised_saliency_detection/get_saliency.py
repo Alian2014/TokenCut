5ba1e2c013a62893646a0c5f26a2a0fa2923712f
Thops <466007246@qq.com>
2025-09-16 18:02:04 +0800

    初步精读

--- unsupervised_saliency_detection/get_saliency.py
+++ unsupervised_saliency_detection/get_saliency.py
@@ -22,27 +22,67 @@ import torch
 
 # Image transformation applied to all images
 # 确保任何输入到 TokenCut 流程的图像，都和当初训练 ViT 模型时使用的 ImageNet 图像具有相似的数据分布
+# 后续需要和 BoostAdapter 中所用的适用于 CLIP 的参数统一
 ToTensor = transforms.Compose([
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.485, 0.456, 0.406),
                                                      (0.229, 0.224, 0.225)),])
 
+
+'''
+get_tokencut_binary_map:                接收一个图片路径和相关模型参数，输出一个二值的分割图（掩码）和用于可视化的特征向量。
+
+img_pth:                                字符串，需要进行分割的图片路径。
+
+backbone:                               预训练好的骨干网络，即论文中提到的自监督视觉Transformer (ViT) 模型，用于提取特征。
+
+patch_size:                             整数，将图片分割成的图像块 (patch) 的大小（例如 16，代表 16x16 像素）。
+
+tau:                                    浮点数，即论文中的 相似度阈值 τ，在构建图时用于判断两个节点（图像块）之间是否存在强连接
+'''
+
+
 def get_tokencut_binary_map(img_pth, backbone,patch_size, tau) :
+    # 函数使用 Pillow 库从 img_pth 路径打开图片，并用 .convert('RGB') 确保它是标准的3通道彩色图像
     I = Image.open(img_pth).convert('RGB')
+    # 调用一个工具函数 utils.resize_pil 来调整图像尺寸,确保调整后的图像尺寸 (w, h) 能够被 patch_size 整除
     I_resize, w, h, feat_w, feat_h = utils.resize_pil(I, patch_size)
 
+    # 将图像转换为PyTorch张量，并进行标准化，增加一个维度，最后转移到GPU上
     tensor = ToTensor(I_resize).unsqueeze(0).cuda()
+    # 将预处理好的张量送入 backbone 中，进行特征提取，生成一个特征向量
     feat = backbone(tensor)[0]
 
+    # tokencut.ncut 函数利用上一步提取出的特征 feat 来构建一个图，然后应用归一化切割算法
+    # 算法会计算出图的第二小特征向量 (eigvec)，得到一个二值的分割图 (bipartition)
+    # 同时返回原始特征向量 eigvec
     seed, bipartition, eigvec = tokencut.ncut(feat, [feat_h, feat_w], [patch_size, patch_size], [h,w], tau)
+    # bipartition: 二值分割图。这是一个二维数组或张量，其中的值（例如0和1）代表了每个图像块被归为背景还是前景。这就是论文中提到的粗糙掩码 (coarse mask)。
+    # eigvec: 原始的特征向量。这个向量可以被重塑并可视化为一张热力图，直观地展示出模型认为图像的哪些区域是“显著”的，即论文中的“特征注意力 (eigen-attention)”。
     return bipartition, eigvec
 
-def mask_color_compose(org, mask, mask_color = [173, 216, 230]) :
 
+'''
+mask_color_compose:             将分割算法产生的掩码（mask）以指定的半透明颜色叠加到原始图像上，从而生成一个直观、漂亮的分割结果图。
+
+org:                            NumPy数组格式的原始图像。
+
+mask:                           NumPy数组格式的分割掩码，通常是get_tokencut_binary_map函数返回的bipartition。
+
+mask_color:                     一个包含RGB三个值的列表，代表了叠加掩码时使用的颜色。默认值 [173, 216, 230] 是淡蓝色
+'''
+
+
+def mask_color_compose(org, mask, mask_color = [173, 216, 230]) :
+    # 这一步将输入的 mask 进行二值化处理,> 0.5 的为 True
     mask_fg = mask > 0.5
+    # 为了不修改原始输入的图像数据，这里创建了一个原始图像 org 的副本 rgb。后续所有的修改都将在这个副本上进行。
     rgb = np.copy(org)
+    # 颜色混合与叠加
     rgb[mask_fg] = (rgb[mask_fg] * 0.3 + np.array(mask_color) * 0.7).astype(np.uint8)
 
+    # 最后，函数将修改后的NumPy数组 rgb 转换回 Pillow 库的图像（Image）对象，并将其返回。
+    # 这个返回的图像就可以直接显示或保存
     return Image.fromarray(rgb)
 
 
